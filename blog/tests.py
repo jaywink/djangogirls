b426fd102c865e502b5d9f5f4d0f2b6cc7d47d5b
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2014-12-13 15:57:52 +0200

    Add examples for Django basic TestCase
    Tests the views a bit

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,54 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Post
+
+
+class SuperUserTestCase(TestCase):
+    def setUp(self):
+        """Add some objects to the database that all tests will need."""
+        self.user = User.objects.create_superuser(
+            username='admin', email='', password='admin'
+        )
+        self.post = self._create_post(
+            author=User.objects.get(username='admin'),
+            title='Test Title 1',
+            text='Here is lots of post content... blah blah blah...'
+        )
+
+        self.client.login(username='admin', password='admin')
+
+    def test_superuser_can_view_homepage(self):
+        response = self.client.get(reverse('post_list'))
+        self.assertContains(response, 'Django Girls Blog')
+
+    def test_superuser_can_view_blog_post(self):
+        response = self.client.get(reverse('post_detail', kwargs={'pk': self.post.pk}))
+        self.assertContains(response, self.post.title)
+
+    def test_superuser_can_add_new_blog_post(self):
+        title = 'Test Title 2'
+        text = 'Here is lots of post content... blah blah blah...'
+
+        response = self.client.post(reverse('post_new'), {
+            'title': title,
+            'text': text,
+        }, follow=True)
+
+        post = Post.objects.get(title=title)
+
+        self.assertRedirects(response, reverse('post_detail', kwargs={'pk': post.pk}))
+        self.assertContains(response, title)
+
+    def test_superuser_can_edit_old_blog_post(self):
+        # TODO: Write a test to check if the admin can edit a previous post.
+        pass
+
+    def _create_post(self, author, title='Test Title 1', text='Lorem ipsum'):
+        return Post.objects.create(
+            author=author,
+            title=title,
+            text=text,
+        )
--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from blog import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='post_list'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 )
