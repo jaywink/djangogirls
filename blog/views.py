6fd84356a83ac466158db8569cd386b8f560a7db
Jason Robinson <jason.robinson@andersinno.fi>
2014-12-14 15:56:27 +0200

    Show unpublished posts too on blog list for superuser - breaks previous commit test

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,10 @@ from blog.models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
+    if request.user.is_staff:
+        posts = Post.objects.all().order_by('published_date')
+    else:
+        posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
     return render(request, 'blog/post_list.html', {"posts": posts})
 
 
