55d7aa548828bd8940f554b254d8ce08673bf2be
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2014-12-15 21:33:58 +0200

    Simplify examples

--- unittest_examples/will_the_snow_melt_3.py
+++ unittest_examples/will_the_snow_melt_3.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 def parse_integer(string_in):
     """Return an integer from a string or else None."""
-    if isinstance(string_in, str):
-        string_in = string_in.strip()
     try:
         return int(string_in)
     except ValueError:
-        return False
+        return False # We decide that we'd prefer to return False here.
 
 
 def will_the_snow_melt_today(temperature_string):
